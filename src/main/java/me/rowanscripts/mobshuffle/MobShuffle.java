32a0151a009a24f4a5a6ce56f63249126925cbdf
icallhacks <rowandoorneweerd9@gmail.com>
2022-09-26 19:40:19 +0200

    Initial commit

---
+++ src/main/java/me/rowanscripts/mobshuffle/MobShuffle.java
@@ -0,0 +1,17 @@
+package me.rowanscripts.mobshuffle;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MobShuffle extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
